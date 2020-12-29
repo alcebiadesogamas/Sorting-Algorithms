50ff71d72f27b6965aa9572987d406bd4d0cd0be
Alcebiades L. Gamas <49372111+alcebiadesogamas@users.noreply.github.com>
2020-12-29 10:54:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ while flag:
             alvo[i],alvo[i+1] = alvo[i+1], alvo[i]
 print(f'{"Bubble Sort":#^30}')
 print(alvo)
+
+############################################
+
+for i in range(1,len(alvo)):
+    indexMenor = i-1
+    for j in range(i, len(alvo)):
+        if alvo[j] < alvo[indexMenor]:
+            indexMenor = j
+    alvo[i-1],alvo[indexMenor] = alvo[indexMenor], alvo[i-1]
+print(f'{"Insertion Sort":#^30}')    
+print(alvo)
